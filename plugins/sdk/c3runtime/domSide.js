742639b41e52175c33edf7ad940805cbce52405a
koteelok <koteeldev@gmail.com>
2024-03-22 19:56:28 +0300

    Fix ysdk initialization

--- plugins/sdk/c3runtime/domSide.js
+++ plugins/sdk/c3runtime/domSide.js
@@ -140,6 +140,7 @@
       return new Promise((resolve) => {
         const head = document.getElementsByTagName('head')[0];
         const script = document.createElement('script');
+        script.src = 'https://yandex.ru/games/sdk/v2';
         script.async = true;
         script.onload = async () => resolve();
         head.appendChild(script);
